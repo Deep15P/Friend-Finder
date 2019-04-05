1f712bb7c851668a7e03727bed5ee8789453465f
Planet15 <parmar0815@gmail.com>
2019-04-04 22:32:52 -0400

    friends data created

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var matches = [
+    {
+        name: "Selena Gomez",
+        photo: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/bgus-1486785-002-1550157188.jpg?crop=1.00xw:0.335xh;0,0.0170xh&resize=1200:*",
+        scores: [5, 3, 4, 2, 1, 5, 3, 1, 2, 5]
+    },
+    {
+        name: "Sara Sampaio",
+        photo: "https://archive.mid-day.com/photos/plog-content/images/2018/oct/10/Sara-sampaio-h_l.jpg",
+        scores: [4, 3, 2, 2, 4, 3, 5, 2, 4, 3]
+    },
+    {
+        name: "Adriana Lima",
+        photo: "https://akns-images.eonline.com/eol_images/Entire_Site/2019124/rs_634x1024-190224215742-634.adriana-lima-vanity-fair-oscars-party-2019.ct.022419.jpg",
+        scores: [2, 4, 1, 5, 3, 3, 4, 5, 5, 2]
+    },
+    {
+        name: "Eiza Gonzalez",
+        photo: "https://www.theplace2.ru/archive/eiza_gonzalez/img/eiza_gonzalez_2018_women_in_film_pre_oscar_cocktail_party_in_la_2.jpg",
+        scores: [3, 5, 3, 4, 5, 2, 1, 3, 1, 4]
+    },
+    {
+        name: "Rene Borisova",
+        photo: "https://scontent-lga3-1.cdninstagram.com/vp/fc39f461eb155d19dd0a108cd7c08c96/5D1562A3/t51.2885-15/e35/51820258_187369568903369_3691323933560767745_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com",
+        scores: [3, 5, 3, 4, 5, 2, 1, 3, 1, 4]
+    }
+];
+
+module.exports = matches;
